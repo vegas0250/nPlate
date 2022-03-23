916b64289b273572bdfc83061ed4c374c4ff8142
vegas0250 <vegas0250@gmail.com>
2022-03-24 03:38:33 +0500

    append images, and little style

--- assets/src/n-plate.js
+++ assets/src/n-plate.js
@@ -10,24 +10,52 @@ function nPlate (elem, config) {
 
         var nPlatePaginatorFirst = document.createElement('div');
         nPlatePaginatorFirst.setAttribute('class', 'n-plate__paginator-first');
-
+        
+        var nPlatePaginatorFirstSvg = document.createElement('img');
+        nPlatePaginatorFirstSvg.setAttribute('src', 'img/page-first.svg');
+        
         var nPlatePaginatorPrev = document.createElement('div');
         nPlatePaginatorPrev.setAttribute('class', 'n-plate__paginator-prev');
 
+        var nPlatePaginatorPrevSvg = document.createElement('img');
+        nPlatePaginatorPrevSvg.setAttribute('src', 'img/angle-left.svg');
+
         var nPlatePaginatorContent = document.createElement('div');
         nPlatePaginatorContent.setAttribute('class', 'n-plate__paginator-content');
+        nPlatePaginatorContent.innerHTML = '1';
 
         var nPlatePaginatorNext = document.createElement('div');
         nPlatePaginatorNext.setAttribute('class', 'n-plate__paginator-next');
 
+        var nPlatePaginatorNextSvg = document.createElement('img');
+        nPlatePaginatorNextSvg.setAttribute('src', 'img/angle-right.svg');
+
         var nPlatePaginatorLast = document.createElement('div');
         nPlatePaginatorLast.setAttribute('class', 'n-plate__paginator-last');
+
+        var nPlatePaginatorLastSvg = document.createElement('img');
+        nPlatePaginatorLastSvg.setAttribute('src', 'img/page-last.svg');
 
         var nPlateReload = document.createElement('div');
         nPlateReload.setAttribute('class', 'n-plate__reload');
 
+        var nPlateReloadSvg = document.createElement('img');
+        nPlateReloadSvg.setAttribute('src', 'img/reload.svg');
+
         var nPlateSetting = document.createElement('div');
         nPlateSetting.setAttribute('class', 'n-plate__setting');
+
+        var nPlateSettingSvg = document.createElement('img');
+        nPlateSettingSvg.setAttribute('src', 'img/setting.svg');
+
+        nPlatePaginatorFirst.appendChild(nPlatePaginatorFirstSvg);
+        nPlatePaginatorPrev.appendChild(nPlatePaginatorPrevSvg);
+        nPlatePaginatorNext.appendChild(nPlatePaginatorNextSvg);
+        nPlatePaginatorLast.appendChild(nPlatePaginatorLastSvg);
+
+        nPlateReload.appendChild(nPlateReloadSvg);
+
+        nPlateSetting.appendChild(nPlateSettingSvg);
 
         nPlatePaginator.appendChild(nPlatePaginatorFirst);
         nPlatePaginator.appendChild(nPlatePaginatorPrev);
@@ -46,10 +74,6 @@ function nPlate (elem, config) {
 
     this.buildTable = function() {
 
-        console.log(this.config.columns);
-
-        
-
         var nPlateTable = document.createElement('table');
         nPlateTable.setAttribute('class', 'n-plate__table');
 
@@ -61,6 +85,8 @@ function nPlate (elem, config) {
             var nPlateTableTHeadTh = document.createElement('th');
             var nPlateTableTHeadThDiv = document.createElement('div');
             var nPlateTableTHeadThInput = document.createElement('input');
+            nPlateTableTHeadThInput.setAttribute('name', i);
+            nPlateTableTHeadThInput.setAttribute('type', 'text');
 
             nPlateTableTHeadThDiv.innerHTML += i;
 
@@ -90,6 +116,8 @@ function nPlate (elem, config) {
 
     this.load = function() {}
 
+
+    // start 
     this.elem = document.querySelector(elem);
     this.config = config;
 
@@ -98,7 +126,10 @@ function nPlate (elem, config) {
         'limit': 10
     }))
 
-    console.log(this.elem);
+    this.buildControl();
+    this.buildTable();
+    this.buildRows();
+
 
     /*
     {
@@ -110,9 +141,4 @@ function nPlate (elem, config) {
         'size': 10
     }
     */
-
-    this.buildControl();
-    this.buildTable();
-    this.buildRows();
-
 }
